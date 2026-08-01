f5a0159f367f8ab6467e1fbe85a9a68320788c63
tiaow <3696812003@qq.com>
2026-08-01 18:20:21 +0800

    更新 DM.lua

--- DM.lua
+++ DM.lua
@@ -2,9 +2,47 @@ local library = loadstring(game:HttpGet("https://github.com/tiaow/gb/blob/0fdfac
 local window = library:new("DW")
 
 local Xing = window:Tab("信息",'')
-local Playerm = window:Tab("玩家",'')
 local Main = window:Tab("主要",'')
-local Cheng = window:Tab("成就",'')
 
-Xing:Lable
+Xing1:Label("作者:idk")
+ Xing1:Label("应该不会ban（maybe）")
 
+local Xing2 = Xing:section("设置",true)
+        Xing2:Toggle("移除UI辉光", "", false, function(state)
+            if state then
+                game:GetService("CoreGui")["frosty is cute"].Main.DropShadowHolder.Visible = false
+            else
+                game:GetService("CoreGui")["frosty is cute"].Main.DropShadowHolder.Visible = true
+            end
+    end)
+
+
+        Xing2:Toggle("彩虹UI", "", false, function(state)
+            if state then
+                game:GetService("CoreGui")["frosty is cute"].Main.Style = "DropShadow"
+            else
+                game:GetService("CoreGui")["frosty is cute"].Main.Style = "Custom"
+            end
+    end)
+
+    
+        Xing2:Button("摧毁GUI",function()
+                game:GetService("CoreGui")["frosty is cute"]:Destroy()
+    end)
+
+local Ma = Mian:section("huh",true)
+Ma:Button("现实章节按钮",function()
+game:GetService("Players").LocalPlayer.PlayerGui.MainGui.StageSelectionUI.Right_Button.Visible = true
+end)
+
+Ma:Button("基地无冷却",function()
+print("_____________________________________")
+local A  =  require(game:GetService("ReplicatedStorage").Modules.StatueConfig)
+local Noob = rawget(A , "Noob Tube" )
+for s , b in A do
+rawset(b ,"AbilityCooldown" , 0)
+print(s,b)
+end
+print(Noob)
+rawset(Noob, "AbilityCooldown" ,0)
+end)
